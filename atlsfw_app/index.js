655b8fa6f847d154e15b8729d4c26326ae08597c
Catherine Xie <catx@lawn-128-61-28-75.lawn.gatech.edu>
2023-04-10 16:55:05 -0400

    single artile pull added

--- atlsfw_app/index.js
+++ atlsfw_app/index.js
@@ -3,30 +3,64 @@ import { MongoClient } from "mongodb";
 // Replace the uri string with your MongoDB deployment's connection string.
 const uri = "mongodb+srv://sverma:<passcode>@atlsfw.dgkxf1k.mongodb.net/posts?retryWrites=true&w=majority";
 
-const client = new MongoClient(uri);
+const allArticleClient = new MongoClient(uri);
+const singleArticleClient = new MongoClient(uri);
 
-async function run() {
+/**
+ * This async function finds all the articles in the database and returns their article id, author, preview image, title for previewing the articles in a list format.
+ * @returns an async list of articles present in the database
+ */
+async function pullAllArticlesPreview() {
   try {
-    const database = client.db("posts");
+    const database = allArticleClient.db("posts");
     const articles = database.collection("articles");
-
-    // Query for a movie that has the title 'The Room'
+    //Store list of articles
+    const allArticles = []
+    // Specific filter for the query. If empty, then it finds all.
     const query = {};
 
     const options = {
-      // sort matched documents in descending order by rating
-      // Include only the `title` and `imdb` fields in the returned document
-      projection: { _id: 0, author: 1 },
+      // Include the fields needed for previews in the returned documents
+      projection: { _id: 1, author: 1, title: 1, preview_image: 1 },
     };
 
     const cursor = await articles.find(query, options);
 
     await cursor.forEach(article => {
-      console.log(article);
+      allArticles.push(article)
     });
 
+    return allArticles
   } finally {
-    await client.close();
+    allArticleClient.close()
   }
 }
-run().catch(console.dir);
+
+/**
+ * 
+ * @param {*} id refers to the article id that we are performing a search one.
+ * @returns an article object with the corresponding id. -1 if nothing was found.
+ */
+async function findArticleWithId(id) {
+  try {
+    const database = singleArticleClient.db("posts");
+    const articles = database.collection("articles");
+
+    // Specific filter for the query. If empty, then it finds all.
+    const query = { _id : id };
+
+    const options = {
+      // Include all fields in the returned document
+      projection: { _id: 1, author: 1 ,title: 1, preview_image: 1, content: 1},
+    };
+
+    const article = await articles.findOne(query, options);
+    if (article == null) {
+      return -1;
+    }
+    return article
+  } finally {
+    singleArticleClient.close()
+  }
+}
+
