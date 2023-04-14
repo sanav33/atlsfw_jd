f7ce074763c9f3dd7d440d0d2332cba6d9e35c95
echen312 <echen312@gatech.edu>
2023-04-13 22:05:15 -0400

    heart increases like count, got from db

--- client/App.js
+++ client/App.js
@@ -1,31 +1,43 @@
 // This is example code to hit the server endpoint to access the posts in JSON format.
-/*
+
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Text, Image } from 'react-native';
+import { Button, View, StyleSheet, Text, Image, Pressable } from 'react-native';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: 50,
+    alignItems: "center",
+    justifyContent: "center",
   },
   image: {
-    width: 50,
-    height: 50,
+    width: 300,
+    height: 300,
   },
   logo: {
     width: 66,
     height: 58,
   },
+  likes: {
+    flexDirection: 'row',
+    paddingTop: 1
+  }
 });
 
-const DisplayAnImage = () => {
+const LikeButton = () => {
+  const [liked, setLiked] = useState(false);
   const [ip, setIp] = useState('');
+  const [count, setCount] = useState(1);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("http://<DEV_SERVER_IP>/posts/");
+        const response = await fetch("http://128.61.53.208:5050/posts/6434998aac3580d94d2d9858");
         const data = await response.json();
-        setIp(data);
+        // count = data.likes;
+        // console.log(data.likes);
+        setCount(data.likes);
+        setIp(data.likes);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -35,11 +47,28 @@ const DisplayAnImage = () => {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={{ uri: "https://images.unsplash.com/photo-1555685812-4b943f1cb0eb?auto=format&fit=crop&w=700&q=60" }}/>
-</View>
+    <Pressable onPress={() => setLiked((isLiked) => !isLiked)} style={styles.likes}>
+      <MaterialCommunityIcons
+        name={liked ? "heart" : "heart-outline"}
+        size={32}
+        color={liked ? "red" : "black"}
+      />
+      <Text>{liked ? (count + 1) : count}</Text>
+    </Pressable>
+    
   );
 };
 
+const DisplayAnImage = () => {
+
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={{ uri: "https://images.unsplash.com/photo-1555685812-4b943f1cb0eb?auto=format&fit=crop&w=700&q=60" }}/>
+      <LikeButton/>
+    </View>
+  );
+};
+
+
+
 export default DisplayAnImage;
-*/
