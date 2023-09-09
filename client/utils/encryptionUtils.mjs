7a58cb9ec597e072da720d1a2129030e8621dec2
CatherineX110 <76015588+CatherineX110@users.noreply.github.com>
2023-09-08 20:34:30 -0400

    Update encryptionUtils.mjs based on comment

--- client/utils/encryptionUtils.mjs
+++ client/utils/encryptionUtils.mjs
@@ -2,7 +2,7 @@ import publicKeyPem from "../pki.mjs";
 import forge from 'node-forge';
 
 /*
- * This function takes in a string and return an encrypted string based on the public key
+ * This function accepts a string and returns an encrypted string
  */
 function encryptWithPublicKey(message) {
     const publicKey = forge.pki.publicKeyFromPem(publicKeyPem);
