fa1cf28bc0c40a0ee23788261631912fa3414bcb
Catherine Xie <catx@lawn-128-61-115-236.lawn.gatech.edu>
2023-09-08 20:12:52 -0400

    Added documentation for encryption

--- client/utils/encryptionUtils.mjs
+++ client/utils/encryptionUtils.mjs
@@ -1,6 +1,9 @@
 import publicKeyPem from "../pki.mjs";
 import forge from 'node-forge';
 
+/*
+ * This function takes in a string and return an encrypted string based on the public key
+ */
 function encryptWithPublicKey(message) {
     const publicKey = forge.pki.publicKeyFromPem(publicKeyPem);
     const encrypted = publicKey.encrypt(message, 'RSA-OAEP', {
