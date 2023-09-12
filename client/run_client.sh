fd9a98cacaa7b2c7bbce277fbd067563e3583961
sanav33 <sanyaverma0303@gmail.com>
2023-09-12 15:46:21 -0400

    add script

---
+++ client/run_client.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Determine the operating system
+OS="$(uname)"
+MY_LOCAL_IP=""
+
+if [ "$OS" == "Linux" ]; then
+    MY_LOCAL_IP=$(hostname -I | awk '{print $1}')
+    # Your Linux-specific command here
+elif [ "$OS" == "Darwin" ]; then
+    MY_LOCAL_IP=$(ifconfig en0 | grep "inet" | awk '{print $2}')
+    # Your macOS-specific command here
+else
+    echo "This operating system is not supported by the script."
+    exit 1
+fi
+
+echo -e "export const MY_IP_ADDRESS = \"$MY_LOCAL_IP\";\nexport default MY_IP_ADDRESS;" > ./environment_variables.mjs && npx expo start
