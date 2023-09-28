c9fccc3a58674abbd5d3ef17597ce7f8b831d0ec
Sana Verma <sanyaverma0303@gmail.com>
2023-09-27 21:58:41 -0400

    add success response

--- server/routes/posts.mjs
+++ server/routes/posts.mjs
@@ -36,7 +36,6 @@ router.post('/posts/:user_id/:article_id/', async (req, res) => {
   const like = parseInt(req.query.like);
   const { liked_articles } = req.body;
 
-  console.log("user_id: ", user_id, "article_id: ", article_id, "like: ", like, "liked_articles: ", liked_articles);
   if (!user_id) {
       return res.status(404).send({ message: "User not found!" });
   }
@@ -44,24 +43,25 @@ router.post('/posts/:user_id/:article_id/', async (req, res) => {
       return res.status(404).send({ message: "Article not found!" });
   }
 
-  const result = await users_db.collection("customer_info").updateOne({ _id: new ObjectId(user_id) }, { $set: { liked_articles: liked_articles }});
-  console.log(result);
-
-  const options = {
-    // Include all fields in the returned document
-    projection: { like_count: 1 },
-  };
+  await users_db.collection("customer_info").updateOne({ _id: new ObjectId(user_id) }, { $set: { liked_articles: liked_articles }},
+    (err, _) => {
+      if (err) {
+        return res.status(400).send({ success: false, message: "Liked article update failed!" });
+      }
+  });
 
   // Update likes based on the "like" query parameter
   let like_count = 0;
   if (like && (like === 1 || like === -1)) {
-      like_count = await posts_db.collection("articles").updateOne({ _id: new ObjectId(article_id) }, { $inc: { like_count: like }});
+      await posts_db.collection("articles").updateOne({ _id: new ObjectId(article_id) }, { $inc: { like_count: like }}, (err, _) => {
+      if (err) {
+        return res.status(400).send({ success: false, message: "Like action unsuccessful!" });
+      }});
   } else {
       return res.status(400).send({ message: "Invalid like query!" });
   }
-  console.log(like_count);
 
-  return res.send({ like_count: like_count });
+  return res.send({ success: true });
 });
 
 
