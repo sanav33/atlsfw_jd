116a7ccbb42b0ebb2d04fab4c9192372a3b41b82
Kim, Corinne Mimi <corinnekim1031@gmail.com>
2023-10-03 01:30:54 -0400

    fixed like and save buttons and author name

--- client/components/Article.js
+++ client/components/Article.js
@@ -1,14 +1,29 @@
-import {View, Image, Text, StyleSheet, Pressable} from "react-native";
+import {View, Image, Text, StyleSheet, Pressable, TouchableOpacity} from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import {useState, useEffect} from "react";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AuthorNameScreen from '../Screens/AuthorNameScreen'; // Import the AuthorNameScreen component
 
 const Article = (props) => {
-	const { image, title } = props.article;
+	const { image, title, author} = props.article;
+
+	const [currentScreen, setCurrentScreen] = useState('Community');
 
 	const [ratio, setRatio] = useState(1);
+	const [isSavePressed, setSavePressed] = useState(false);
+  const [liked, setLiked] = useState(false);
+  const [count, setCount] = useState(1);
 
 	const LikeButton = () => {};
 
+	const navigateToPage = (pageName) => {
+        setCurrentScreen(pageName);
+    };
+
+    const navigateToAuthorPage = () => {
+      setCurrentScreen('AuthorNameScreen');
+    };
 	useEffect(() => {
 		if (image) {
 			Image.getSize(image, (width, height) => setRatio(width / height));
@@ -24,14 +39,22 @@ const Article = (props) => {
 					}}
 					style={[styles.image, {aspectRatio: ratio}]}
 				/>
-				<Pressable onPress={LikeButton} style={styles.likeButton}>
-      			<AntDesign
-        			name={"hearto"}
-        			size={20}
-        			color={"black"}
-      			/>
-    			</Pressable>
 				<Text style={styles.title}>{title}</Text>
+				<TouchableOpacity onPress={navigateToAuthorPage} style={{ marginTop: 10 }}>
+            <Text style={styles.authorName}>{author}</Text>
+         </TouchableOpacity>
+         				<Pressable onPress={() => setLiked((isLiked) => !isLiked)} style={styles.likeButton}>
+      				<MaterialCommunityIcons
+        			name={liked ? "heart" : "heart-outline"}
+        			size={32}
+        			color={liked ? "red" : "black"}
+      				/>
+      			<Text>{liked ? (count + 1) : count}</Text>
+    			</Pressable>
+
+          		<TouchableOpacity onPress={() => setSavePressed((isSavePressed) => !isSavePressed)} style={styles.saveButton}>
+            		<Icon name={isSavePressed ? 'bookmark' : 'bookmark-o'} size={30} color={isSavePressed ? 'blue' : 'black'} />
+         		</TouchableOpacity>
 			</View>
 		</View>
 
@@ -46,19 +69,33 @@ const styles = StyleSheet.create({
 		fontSize: 16,
 		fontWeight: "bold",
 		margin: 10,
+		left: 10,
 	},
 	image: {
 		width: "100%",
 		borderRadius: 25,
 	},
 	likeButton: {
-		backgroundColor: "#D3CFD4",
+		// backgroundColor: "#D3CFD4",
 		position: "absolute",
-		bottom: 10,
+		bottom: 0,
 		right: 10,
 		padding: 5,
 		borderRadius: 50,
 	},
+	saveButton: {
+		position: "absolute",
+		bottom: 22,
+		right: 48,
+		padding: 5,
+		borderRadius: 50,
+	},
+	authorName: {
+		textDecorationLine: 'underline',
+		left: 10,
+		marginBottom: 20,
+	}
 });
 
 export default Article
+
