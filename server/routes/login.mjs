da24922550e53cfd0cce06d438396a438ccc3e0c
Catherine Xie <catx@lawn-128-61-116-167.lawn.gatech.edu>
2023-10-16 15:42:12 -0400

    adding user info to login/signup responses

--- server/routes/login.mjs
+++ server/routes/login.mjs
@@ -29,7 +29,11 @@ router.post("/", async (req, res) => {
         console.log(hashed_password);
         return res.status(400).json({ success: false, message: 'The email-password combination is incorrect' });
     }
-    res.json({ success: true });
+
+    const userInfo = await users_db.collection("customer_info").findOne({hashed_email: hashed_email});
+    userInfo._id = userInfo._id.toString();
+
+    res.status(200).json({ success: true, user: userInfo});
 });
 
 
