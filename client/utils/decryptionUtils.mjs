ec66cad40f0f71f644ddbd7fd20e934d035a4b74
Catherine Xie <catx@lawn-128-61-118-30.lawn.gatech.edu>
2023-11-15 13:43:43 -0500

    adding decryption

---
+++ client/utils/decryptionUtils.mjs
@@ -0,0 +1,23 @@
+import { privateKeyPem } from '../password.mjs';
+import forge from 'node-forge';
+
+export function decryptWithPrivateKey(encryptedMessage) {
+    // Convert the PEM-formatted private key to a private key object
+    const privateKey = forge.pki.privateKeyFromPem(privateKeyPem);
+    
+
+    // Decode the base64-encoded encrypted message
+    const encryptedBuffer = forge.util.decode64(encryptedMessage);
+
+    // Decrypt the buffer using the private key
+    const decrypted = privateKey.decrypt(encryptedBuffer, 'RSA-OAEP', {
+        md: forge.md.sha256.create(),
+        mgf1: {
+            md: forge.md.sha1.create()
+        }
+    });
+
+    return decrypted;
+}
+
+export default decryptWithPrivateKey;
