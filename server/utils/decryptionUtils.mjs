905d6c8ccc680e94c60ef62b9e742157eae3e1fc
Catherine Xie <catx@lawn-128-61-114-177.lawn.gatech.edu>
2023-12-07 12:58:07 -0500

    fixes

--- server/utils/decryptionUtils.mjs
+++ server/utils/decryptionUtils.mjs
@@ -1,5 +1,20 @@
 import { privateKeyPem } from '../password.mjs';
 import forge from 'node-forge';
+import { publicKeyPem } from '../password.mjs';
+
+/*
+ * This function accepts a string and returns an encrypted string
+ */
+export function encryptWithPublicKey(message) {
+    const publicKey = forge.pki.publicKeyFromPem(publicKeyPem);
+    const encrypted = publicKey.encrypt(message, 'RSA-OAEP', {
+        md: forge.md.sha256.create(),
+        mgf1: {
+            md: forge.md.sha1.create()
+        }
+    });
+    return forge.util.encode64(encrypted);
+}
 
 export function decryptWithPrivateKey(encryptedMessage) {
     // Convert the PEM-formatted private key to a private key object
